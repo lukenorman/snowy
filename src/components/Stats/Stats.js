03083331ace00348d18f31842f767c9087dc0189
Luke Norman <lukenorman@gmail.com>
2020-01-23 12:37:44 -0800

    Added most days skied

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -7,12 +7,14 @@ function Stats() {
     const [epicDays, setEpicDays] = useState(0)
     const [ikonDays, setIkonDays] = useState(0)
     const [indyDays, setIndyDays] = useState(0)
+    const [mostDays, setMostDays] = useState([])
     const [resortsSkied, setResortsSkied] =  useState(0)
 
     useEffect(() => {
         let epic = 0;
         let ikon = 0;
         let indy = 0
+        let mostDaysSkied = [];
         let resorts = [];
         for (let day of Days) {
             let resort = Resorts.find(r => r.name === day.resort)
@@ -30,11 +32,18 @@ function Stats() {
                 }
             }
         }
+        for (let resort of resorts) {
+            let days = Days.filter(d => d.resort === resort.name)
+            if (days.length > mostDaysSkied.length) {
+                mostDaysSkied = days;
+            }
+        }
         setEpicDays(epic)
         setIkonDays(ikon)
         setIndyDays(indy)
         setResortsSkied(resorts.length)
-    }, [setEpicDays, setIkonDays]);
+        setMostDays(mostDaysSkied)
+    }, [setEpicDays, setIkonDays, setIndyDays, setResortsSkied, setMostDays]);
 
     return (<div>
                 <h2>Stats</h2>
@@ -43,6 +52,7 @@ function Stats() {
                 <div>Ikon Days: {ikonDays} </div>
                 <div>Indy Days: {indyDays} </div>
                 <div>Resort Skied: {resortsSkied}</div>
+                <div>Most Days: {mostDays.length} @ {(mostDays[0] && mostDays[0].resort) || ""} </div>
             </div>)
 }
 
