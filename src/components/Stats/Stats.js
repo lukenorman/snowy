31869ee008ff3e2d86b195c06d8851c8501374ae
Luke Norman <lukenorman@gmail.com>
2020-02-20 20:52:51 -0800

    Add some fresh stats

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -9,6 +9,23 @@ function Stats() {
     const [indyDays, setIndyDays] = useState(0)
     const [mostDays, setMostDays] = useState([])
     const [resortsSkied, setResortsSkied] =  useState(0)
+    const [oneDay, setOneDay] = useState([])
+    const [daysInARow, setDaysInARow] = useState(0)
+    const [daysInARowStart, setDaysInARowStart] = useState(null)
+
+    const datesEqual = (date,startDate,days) => {
+        let secondDate = new Date(startDate)
+        secondDate.setDate(secondDate.getDate() + days);
+        let equal = date.getFullYear() === secondDate.getFullYear() 
+        && date.getMonth() === secondDate.getMonth() 
+        && date.getDate() === secondDate.getDate();
+        return equal
+    }
+
+    const printDate = date => {
+        if (date === null) return ""
+        else return date.toDateString()
+    }
 
     useEffect(() => {
         let epic = 0;
@@ -16,7 +33,23 @@ function Stats() {
         let indy = 0
         let mostDaysSkied = [];
         let resorts = [];
+        let oneDayWonders = [];
+        let maxConsecutive = 0
+        let maxConsecutiveStart = new Date(Days[0].date)
+        let consecutiveDays = 0;
+        let startDate = new Date(Days[0].date);
         for (let day of Days) {
+            let date = new Date(day.date)
+            if (datesEqual(date,startDate,consecutiveDays)) {
+                consecutiveDays++
+            } else {
+                if (consecutiveDays > maxConsecutive) {
+                    maxConsecutive = consecutiveDays
+                    maxConsecutiveStart = startDate
+                }
+                consecutiveDays = 1
+                startDate = new Date(day.date)
+            }
             let resort = Resorts.find(r => r.name === day.resort)
             if (resort) {
                 if (!resorts.includes(resort)) {
@@ -37,22 +70,30 @@ function Stats() {
             if (days.length > mostDaysSkied.length) {
                 mostDaysSkied = days;
             }
+            if (days.length === 1) {
+                oneDayWonders.push(resort.name)
+            }
         }
         setEpicDays(epic)
         setIkonDays(ikon)
         setIndyDays(indy)
         setResortsSkied(resorts.length)
         setMostDays(mostDaysSkied)
-    }, [setEpicDays, setIkonDays, setIndyDays, setResortsSkied, setMostDays]);
+        setOneDay(oneDayWonders)
+        setDaysInARow(maxConsecutive)
+        setDaysInARowStart(maxConsecutiveStart)
+    }, [setEpicDays, setIkonDays, setIndyDays, setResortsSkied, setMostDays, setOneDay, setDaysInARow, setDaysInARowStart]);
 
     return (<div>
                 <h2>Stats</h2>
-                <div>Total Days: {Days.length} </div>
-                <div>Epic Days: {epicDays} </div>
-                <div>Ikon Days: {ikonDays} </div>
-                <div>Indy Days: {indyDays} </div>
-                <div>Resort Skied: {resortsSkied}</div>
-                <div>Most Days: {mostDays.length} @ {(mostDays[0] && mostDays[0].resort) || ""} </div>
+                <div><b>Total Days:</b> {Days.length} </div>
+                <div><b>Epic Days:</b> {epicDays} </div>
+                <div><b>Ikon Days:</b> {ikonDays} </div>
+                <div><b>Indy Days:</b> {indyDays} </div>
+                <div><b>Resort Skied:</b> {resortsSkied}</div>
+                <div><b>Most Days At One Resort:</b> {mostDays.length} @ {(mostDays[0] && mostDays[0].resort) || ""} </div>
+                <div><b>Most Days In A Row:</b> {daysInARow} starting @ {printDate(daysInARowStart)}</div>
+                <div><b>1 Day Wonders:</b> {oneDay.join(", ")} </div>
             </div>)
 }
 
